7eef62b23e38a1e1d5a4e8b470232e93a90996a7
Heron Rodrigues <heronrs1@gmail.com>
2019-08-29 17:07:56 -0300

    Creating user register router

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const PostController = require('./controllers/PostController');
 const LikeController = require('./controllers/LikeController');
+const UserController = require('./controllers/UserController');
 const UploadConfig = require('./config/Upload');
 
 const routes = new express.Router();
@@ -11,5 +12,7 @@ routes.get('/posts', PostController.index);
 
 routes.post('/posts', upload.single('image'), PostController.store);
 routes.post('/posts/:id/like', LikeController.store);
+
+routes.post('/register', UserController.index);
 
 module.exports = routes;
